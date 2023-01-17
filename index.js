9f4b39adabdda89c4fcfe0dd34c845f764238f25
Marina <marinashishlyaeva@Marinas-MacBook-Air.local>
2023-01-17 12:54:30 +0100

    add and update package-lock.json index.js package.json

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-console.log('Hello, Hexlet!');
+import fs from "fs";
+import _ from "lodash";
 
+//const data = fs.readFileSync("path/to/file");
+//console.log(data);
+console.log(_.last(['one', 'two']));
